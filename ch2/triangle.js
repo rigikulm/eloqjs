de5de85c5369090d056c3ae8e4367a0045d72340
Chris Lemler <clemler@pm.me>
2020-02-23 14:02:27 -0800

    feat(triangle.js): implemented 'Looping Triangle' exercise

---
+++ ch2/triangle.js
@@ -0,0 +1,17 @@
+// Looping a Triangle
+//
+// Write a loop that makes seven calls to console.log to output the
+// following triangle:
+//
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+//
+
+for (let i = 0; i < 7; i++) {
+    console.log("#".repeat(i+1));
+}

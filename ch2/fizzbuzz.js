6480f7493f35b3d01e8a81c59543d414050fc84f
Chris Lemler <clemler@pm.me>
2020-02-24 20:38:22 -0800

    feat(fizzbuzz.js): implemented the 'fizzbuzz' exercise from chapter 2

---
+++ ch2/fizzbuzz.js
@@ -0,0 +1,31 @@
+// File: fizzbuzz.js
+//
+// Write a program that uses console.log to print all the numbers from 1 to
+// 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead
+// of the number, and for numbers divisible by 5 (and not 3), print "Buzz"
+// instead.
+//
+// When you have that working, modify your program to print "FizzBuzz" for
+// numbers that are divisible by both 3 and 5 (and still print "Fizz" or
+// "Buzz" for numbers divisible by only one of those).
+
+function fizzbuzz(from, to) {
+    for (let i = from; i <= to; i++) {
+        let isModThree = (i % 3) === 0;
+        let isModFive = (i % 5) === 0;
+        if (isModThree && isModFive) {
+            console.log("FizzBuzz");
+        }
+        else if (isModThree) {
+            console.log("Fizz");
+        }
+        else if (isModFive) {
+            console.log("Buzz");
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+
+fizzbuzz(1, 100);
